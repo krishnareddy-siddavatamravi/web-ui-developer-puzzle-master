ae6e563959b27c2890fdbbf36d800eeb29b89d9b
krishnareddy-siddavatamravi <krishnareddy.siddavatamravi@concentrix.com>
2023-08-10 13:39:40 +0530

    Initial commit

---
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  addToReadingList,
+  clearSearch,
+  getAllBooks,
+  ReadingListBook,
+  searchBooks
+} from '@tmo/books/data-access';
+import { FormBuilder } from '@angular/forms';
+import { Book } from '@tmo/shared/models';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'tmo-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.scss']
+})
+export class BookSearchComponent implements OnInit {
+  books: ReadingListBook[];
+
+  searchForm = this.fb.group({
+    term: ''
+  });
+
+  constructor(
+    private readonly store: Store,
+    private readonly fb: FormBuilder
+  ) {
+    this.searchForm.controls.term.valueChanges
+      .pipe(debounceTime(500)) // Debounce input changes for 500ms
+      .subscribe(() => {
+        this.searchBooks(); // Trigger search when input changes
+      });
+  }
+
+  get searchTerm(): string {
+    return this.searchForm.value.term;
+  }
+
+  ngOnInit(): void {
+    this.store.select(getAllBooks).subscribe(books => {
+      this.books = books;
+    });
+  }
+
+  formatDate(date: void | string) {
+    return date
+      ? new Intl.DateTimeFormat('en-US').format(new Date(date))
+      : undefined;
+  }
+
+  addBookToReadingList(book: Book) {
+    this.store.dispatch(addToReadingList({ book }));
+  }
+
+  // searchExample() {
+  //   this.searchForm.controls.term.setValue('javascript');
+  //   this.searchBooks();
+  // }
+
+  // searchBooks() {
+  //   if (this.searchForm.value.term) {
+  //     this.store.dispatch(searchBooks({ term: this.searchTerm }));
+  //   } else {
+  //     this.store.dispatch(clearSearch());
+  //   }
+  // }
+  searchBooks() {
+    const term = this.searchForm.value.term;
+
+    if (term) {
+      this.store.dispatch(searchBooks({ term }));
+    } else {
+      this.store.dispatch(clearSearch());
+    }
+  }
+
+}
